d27e077bcc6f344598e4afaf0ae8d083f4fc97a9
AConn03 <a03connolly@gmail.com>
2026-07-08 09:23:23 -0400

    Implement version check and update banner display
    Add version checking functionality to updater.js

--- js/updater.js
+++ js/updater.js
@@ -1,1 +1,19 @@
+let initialVersion = null;
 
+async function checkVersion() {
+    try {
+        const res = await fetch(`./version.json?_nocache=${Date.now()}`);
+        const data = await res.json();
+        
+        if (!initialVersion) {
+            initialVersion = data.version; 
+        } else if (data.version !== initialVersion) {
+            document.getElementById('updateBanner').style.display = 'block';
+        }
+    } catch (e) { 
+        console.log("Waiting for version file..."); 
+    }
+}
+
+checkVersion();
+setInterval(checkVersion, 5000);
